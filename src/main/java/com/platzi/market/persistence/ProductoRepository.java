a306cef3b4813b549655587a329fe12673ecacc4
cvargas <cristhian.avc21@gmail.com>
2021-03-13 12:46:07 -0500

    Implementar la anotación @Repository

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,18 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Esta es una clase que interactua direcgtamente con la DB, es buena practica decorarla con la anotacion @Repository
+ * Con esto indicamos a Spring que esta clase interactua con la DB ya que aca realizamos las operaciones que queremos aplicar en las tablas
+ * Tambien se puede uutilizar @Component - Esta es una generalizacion, le indicamos que es un componente de spring
+ * es mejor usar @Repository
+ */
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -20,5 +28,20 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return  productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    /**
+     * Puedo eliminar un producto mandando el objeto completo o solo la PK
+     */
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
