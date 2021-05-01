8ac4b269826590b6635a9598963cd54ad428c20b
cvargas <cristhian.avc21@gmail.com>
2021-05-01 09:23:53 -0500

    Autorización con JWT

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.platzi.market.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,5 +16,27 @@ public class JWTUtil {
         return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    //getSubject() es donde esta el usuario de la peticion
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
+
+    /*
+    Verificar si el token ya expiro
+    si la fecha esta antes a la fecha actual, si esta antes es true, sino false (eso quiere decir que el token es aun correcto)
+     */
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    //Objetos dentro del JWT
+    private Claims getClaims(String token){
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
     }
 }
